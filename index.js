83ea28cf537bb3073e1bb375671072dac5a7ab53
tpokora <pokora.tomek@gmail.com>
2015-08-31 21:56:06 +0200

    CORS Support

--- index.js
+++ index.js
@@ -12,9 +12,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
+// CORS Support
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 // Connect to MongoDB
 mongoose.connect('mongodb://tomek:tomek1@ds039088.mongolab.com:39088/testdb');
 mongoose.connection.once('open', function() {
   console.log('Listening on port 3000...');
   app.listen(3000);
-})
+});
