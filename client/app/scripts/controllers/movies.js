ddfc58f6f6cb68b6aa44ff3882c8919bb0506e0d
tpokora <pokora.tomek@gmail.com>
2015-09-05 21:36:03 +0200

    $scope for MoviesCtrl and static data

--- client/app/scripts/controllers/movies.js
+++ client/app/scripts/controllers/movies.js
@@ -8,10 +8,19 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('MoviesCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MoviesCtrl', [ '$scope', function ($scope) {
+    $scope.movies = [
+      {
+        "_id":"55e4b77a8444903418bdefa7",
+        "title":"I Remember You",
+        "url":"https://www.youtube.com/watch?v=OhPFgqHz68o",
+        "__v":0
+      },
+      {
+        "_id":"55e4b8448444903418bdefa9",
+        "title":"How to build a M.E.A.N web application",
+        "url":"https://www.youtube.com/watch?v=OhPFgqHz68o",
+        "__v":0
+      }
     ];
-  });
+  }]);
